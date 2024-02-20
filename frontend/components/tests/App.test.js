fd03a8cd516bee0a3c960cbb0f6b060af922d8bc
Percouple <calebsevan@gmail.com>
2024-02-20 12:09:37 -0600

    Finishing-challenges

--- frontend/components/tests/App.test.js
+++ frontend/components/tests/App.test.js
@@ -1,6 +1,6 @@
 // 👇 YOUR WORK STARTS ON LINE 19
 import React from 'react'
-import { render, waitFor, screen } from '@testing-library/react'
+import { render, waitFor, screen, getByTestId, getByRole } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import server from '../../../backend/mock-server'
@@ -17,27 +17,43 @@ describe('Stranger Things App', () => {
   })
   test('App mounts without crashing', () => {
     // 👉 TASK: print the simulated DOM using screen.debug
+    render(<App />)
   })
   test('App renders the correct texts', async () => {
     // 👉 TASK: click on the button that displays "Press to Get Show Data"
+    user.click(screen.getByText('Press to Get Show Data'))
 
     // 👉 TASK: create a waitFor and await for the following to be true:
-    //    - The text "Press to Get Show Data" is no longer in the DOM
-    //    - The text "Stranger Things" exists in the DOM
-    //    - The text "A love letter to the '80s classics that captivated a generation" exists in the DOM
-    //    - The text "Select A Season" exists in the DOM
-    // ❗ You will need { exact: false } to select the longer text
+    await waitFor(() => {
+      //    - The text "Press to Get Show Data" is no longer in the DOM
+      expect(screen.queryByText("Press to Get Show Data")).toBeNull()
+      //    - The text "Stranger Things" exists in the DOM
+      expect(screen.queryByText('Stranger Things')).toBeVisible()
+      //    - The text "A love letter to the '80s classics that captivated a generation" exists in the DOM
+      // ❗ You will need { exact: false } to select the longer text
+      expect(screen.queryByText("A love letter to the '80s classics that captivated a generation", { exact: false })).toBeVisible()
+      //    - The text "Select A Season" exists in the DOM
+      expect(screen.queryByText('Select A Season')).toBeVisible()
+    })
 
+    
     // 👉 TASK: select Season 2 from the dropdown
     // ❗ Don't forget user actions need the await keyword
     // ❗ Use the selectOptions user action
     // ❗ Grab the select element using querySelector
-
-    // 👉 TASK: create the following assertions:
-    //    - The text "Season 2, Episode 1" exists in the DOM
-    //    - The text "Chapter One: MADMAX" exists in the DOM
-    //    - The text "One year after the events with the Upside Down and the Demogorgon" exists in the DOM
-    // ❗ You will need { exact: false } to select the longer text
+    const dropDown = screen.getByRole('combobox');
+    await user.selectOptions(dropDown, 'Season 2')
+    
+    await waitFor(() => {
+      // 👉 TASK: create the following assertions:
+      //    - The text "Season 2, Episode 1" exists in the DOM
+      expect(screen.queryByText("Season 2, Episode 1")).toBeVisible()
+      //    - The text "Chapter One: MADMAX" exists in the DOM
+      expect(screen.queryByText("Chapter One: MADMAX")).toBeVisible()
+      //    - The text "One year after the events with the Upside Down and the Demogorgon" exists in the DOM
+      expect(screen.queryByText("One year after the events with the Upside Down and the Demogorgon", {exact: false})).toBeVisible()
+      // ❗ You will need { exact: false } to select the longer text
+    })
 
   })
 })
--- frontend/components/tests/Episode.test.js
+++ frontend/components/tests/Episode.test.js
@@ -23,32 +23,64 @@ their own search, and meet a mysterious girl in the forest.",
   url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
 }
 
+const exampleEpisodeDataNoImg = {
+  airdate: "2016-07-15",
+  airstamp: "2016-07-15T12:00:00+00:00",
+  airtime: "",
+  id: 553946,
+  name: "Chapter One: The Vanishing of Will Byers",
+  number: 1,
+  rating: { average: 8.2 },
+  runtime: 49,
+  season: 1,
+  summary: "A young boy mysteriously disappears, and his panicked mother \
+demands that the police find him. Meanwhile, the boy's friends conduct \
+their own search, and meet a mysterious girl in the forest.",
+  type: "regular",
+  url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
+}
+
 describe('Episode component', () => {
   test("renders without error", () => {
     // 👉 TASK: render the component passing episode data
+    render(<Episode />)
 
-    // 👉 TASK: print the simulated DOM using screen.debug
-
+    // 👉 TASK: print the simulated DOM
+    
   })
   test("renders texts and alt texts correctly", () => {
     // 👉 TASK: render the component passing episode data and getting the rerender utility
-
+    render(<Episode episode={exampleEpisodeData}/>)
+    
     // 👉 TASK: check that the summary renders to the DOM
-
+    expect(screen.getByText('A young boy mysteriously disappears', {exact: false})).toBeVisible()
+    
     // 👉 TASK: check that the alt text "episode image" is present
+    const image = screen.getByRole('img')
+    expect(image).toHaveAttribute('alt', "episode image")
 
+    const { rerender } = render(
+      <Episode />
+    );
+    
     // 👉 TASK: rerender the component passing episode data lacking an image
     // ❗ Study the Episode component to understand what happens in this case
-
+    rerender(<Episode episode={exampleEpisodeDataNoImg}/>)
+    
     // 👉 TASK: check that the default image appears in the DOM
     // ❗ Use querySelector to select the image by its src attribute
-
+    expect(document.querySelector('img[src="https://i.ibb.co/2FsfXqM/stranger-things.png"]')).toBeVisible()
+    
     // 👉 TASK: check that the "generic episode image" alt text is present
-
+    expect(screen.getByAltText("generic episode image")).toBeVisible()
+    
     // 👉 TASK: rerender the component passing an undefined episode
     // ❗ Study the Episode component to understand what happens in this case
+    rerender(<Episode episode={undefined} />)
+    screen.debug()
 
     // 👉 TASK: check that the "Loading episode..." text is present
+    expect(screen.getByText('Loading episode...')).toBeVisible()
 
   })
 })
